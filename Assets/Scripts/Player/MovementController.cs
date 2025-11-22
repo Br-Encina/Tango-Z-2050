19f4e247d927058ee472809088cec9bafc1d9ad3
BrunoEncina <encinabruno10@gmail.com>
2025-11-22 04:33:11 -0300

    se borraron algunos scripts del sistema de estados del player

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -1,16 +1,89 @@
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
+using UnityEngine.InputSystem;
 public class MovementController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    InputSystem_Actions action;
+    CharacterController characterController;
+    Animator animator;
+
+    Vector2 currentMovementInput;
+    Vector3 CurrentMovement;
+    bool IsMovementPressed;
+    float rotationFactorPerFrame = 1.0f;
+
+    private void Awake()
     {
-        
+        action = new InputSystem_Actions();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        action.Player.Move.started += onMovementInput;
+        action.Player.Move.canceled += onMovementInput;
+        action.Player.Move.performed += onMovementInput;
+
+    }
+
+    void onMovementInput(InputAction.CallbackContext context)
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        CurrentMovement.z = currentMovementInput.x;
+        //CurrentMovement.z = currentMovementInput.y;
+        IsMovementPressed = currentMovementInput.x != 0; /*|| currentMovementInput.y != 0;*/
+    }
+
+    void handleRotatio()
+    {
+        Vector3 positionTolookAt;
+
+        positionTolookAt.x = CurrentMovement.x;
+        positionTolookAt.y = 0.0f;
+        positionTolookAt.z = CurrentMovement.z;
+
+        Quaternion currentRotation = transform.rotation;
+
+       
+
+        if (IsMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionTolookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame);
+        }
+    }
+
+    void HandleAnimation()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning = animator.GetBool("isRunning");
+
+        if (IsMovementPressed && !isWalking)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else if (!IsMovementPressed && isWalking)
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        handleRotatio();
+        HandleAnimation();
+        characterController.Move(CurrentMovement * Time.deltaTime);
         
+    }
+
+    private void OnEnable()
+    {
+        action.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        action.Player.Disable();
     }
 }
--- Assets/Scripts/Player/States/PlayerBaseState.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-public abstract class PlayerBaseState
-{
-    public abstract void EnterState(PlayerStateMachine player);
-    public abstract void UpdateState(PlayerStateMachine player);
-    public abstract void ExitState(PlayerStateMachine player);
-}
