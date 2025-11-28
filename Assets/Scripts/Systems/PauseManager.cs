c6b807056ef2dd8e8054d2dc4534688363e3d7f9
BrunoEncina <encinabruno10@gmail.com>
2025-11-28 04:24:33 -0300

    Se creo el menu de pausa
    Al apretar la Tecla ESC se actica un panel de pausa

---
+++ Assets/Scripts/Systems/PauseManager.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] GameObject pausePanel;
+
+    [Header("Audio")]
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    bool isPaused = false;
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+
+        // Cargar volumen guardado
+        float musicVal = PlayerPrefs.GetFloat("MusicVolume", 0);
+        float sfxVal = PlayerPrefs.GetFloat("SFXVolume", 0);
+
+        musicSlider.value = musicVal;
+        sfxSlider.value = sfxVal;
+
+        audioMixer.SetFloat("MusicVolume", musicVal);
+        audioMixer.SetFloat("SFXVolume", sfxVal);
+
+        // Eventos sliders
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) ResumeGame();
+            else PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        Scene current = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(current.buildIndex);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        audioMixer.SetFloat("MusicVolume", value);
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        audioMixer.SetFloat("SFXVolume", value);
+        PlayerPrefs.SetFloat("SFXVolume", value);
+    }
+}
