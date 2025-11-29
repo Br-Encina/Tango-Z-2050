6d7448a72663557a8aee0193ecd6909898ba3ea4
Unil2407 <casciofn@gmail.com>
2025-11-29 05:37:29 -0300

    Se hizo la plataforma que cae

---
+++ Assets/Scripts/IntactubleObjects/Falling_Plataform.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class Falling_Plataform : MonoBehaviour
+{
+    private bool falled = false;
+    [SerializeField] private float timer;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (falled) return;
+
+        if (other.gameObject.CompareTag("BoxPlayer"))
+        {
+            StartCoroutine(FallingPlataform());
+            falled = true;
+        }
+    }
+
+    private IEnumerator FallingPlataform()
+    {
+        yield return new WaitForSeconds(timer);
+        rb.isKinematic = false;
+    }
+}
