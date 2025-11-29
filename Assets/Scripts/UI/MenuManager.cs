decb95c90b031e6958313e3e215ff051c3188e6b
BrunoEncina <encinabruno10@gmail.com>
2025-11-29 06:14:33 -0300

    Revert "Merge branch 'V0.1' of https://github.com/Br-Encina/Tango-Z-2050 into V0.1"
    This reverts commit 33138b1328e2fb9ca976fc7d459f8a85e891e6f8, reversing
    changes made to 87787221ad047c2c20a1848b55d72157bd5655af.

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,24 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public Image optionsMenu;
+    
     public void StartGame(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    public void OpenOptionsMenu()
-    {
-        optionsMenu.enabled = true;
-    }
-
-    public void CloseOptionsMenu()
-    {
-        optionsMenu.enabled = false;
-    }
     
     public void QuitGame()
     {
