87787221ad047c2c20a1848b55d72157bd5655af
BrunoEncina <encinabruno10@gmail.com>
2025-11-29 06:13:52 -0300

    se creo el navMesh

---
+++ Assets/Scripts/Systems/CursorManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CursorManager : MonoBehaviour
+{
+    [Header("Cursor cuando apunta a un interactuable")]
+    public Texture2D interactCursor;
+
+    [Header("Hotspot (opcional)")]
+    public Vector2 hotspot = Vector2.zero;
+
+    Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+
+        // Asegurar que el cursor arranque con el original del sistema
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+        HandleCursor();
+    }
+
+    void HandleCursor()
+    {
+        Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100))
+        {
+            var interact = hit.collider.GetComponent<IInteractuable>();
+
+            if (interact != null)
+            {
+                // Cambia al cursor especial de interacción
+                Cursor.SetCursor(interactCursor, hotspot, CursorMode.Auto);
+                return;
+            }
+        }
+
+        // Si no hay nada interactuable → vuelve al cursor normal del sistema
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
